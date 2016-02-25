5da37f883f91714bd8769ece58d7b39a68d44a80
JohanLee-kor <oldmemolee@gmail.com>
2016-02-25 23:11:07 +0900

    date picker debug

--- analysis/urls.py
+++ analysis/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     # url(r'^analysis/',include('analysis.'))
     url(r'^main/$',views.main,name='main'),
     url(r'^delete/$',views.deleteShare,name='delete'),
-    url(r'^(?P<analysisType>[A-Za-z0-9]+)/$', views.analysisShare, name='analysisShare'),#analysis_type 0: R3I, 1: R10T, 2: BOX
     url(r'^analysis2/$',views.analysisShare2,name='analysisShare2'),
+    url(r'^(?P<analysisType>[A-Za-z0-9]+)/$', views.analysisShare, name='analysisShare'),#analysis_type 0: R3I, 1: R10T, 2: BOX
+
 ]
